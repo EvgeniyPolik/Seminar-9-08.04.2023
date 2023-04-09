1dc861bc9b674d7dc13d123973b7cc0e8f41bd4c
poliex <poliex86@gmail.com>
2023-04-09 23:07:35 +0500

    complete

--- task_2.py
+++ task_2.py
@@ -1,3 +1,28 @@
 """
 2) реализовать метакласс. позволяющий создавать всегда один и тот же объект класса (см. урок)
 """
+
+class SingleServer(type):
+    Server = None
+
+    def __call__(cls):
+        if cls.Server is None:
+            cls.Server = super().__call__()
+            return cls.Server
+        return cls.Server
+
+
+class MainServer(metaclass=SingleServer):
+    server_name = 'uni-server'
+    port = 3535
+
+    def __str__(self):
+        return f'Server is running'
+
+
+Server_mess = MainServer()
+Server_app = MainServer()
+Server_web = MainServer()
+
+print(Server_app is Server_mess)
+print(Server_mess is Server_app)
